d63679587c8c6bdee90c1ab0ff89f2d375bbe4dd
Brian Sorahan <brian.sorahan@drillinginfo.com>
2022-01-25 16:05:53 -0600

    testing branch name env var

--- flows/envvar.py
+++ flows/envvar.py
@@ -5,13 +5,16 @@ from prefect.run_configs.kubernetes import KubernetesRun
 from prefect.storage import Git
 
 with Flow("envvar") as flow:
+    branch_name = os.getenv("GITHUB_BRANCH_NAME", "master")
+
     flow.storage = Git(
         repo_host="github.com",
         repo="briansorahan/prefect-playground",
         flow_path="flows/envvar.py",
+        branch_name=branch_name,
     )
     flow.run_config = KubernetesRun(
         image="devprismcr.azurecr.io/acuity/prefect/flows:0.0.47",
     )
-    foo = os.getenv("FOO", "MISSING")
+    foo = os.getenv("FOO with new feature", "MISSING")
     print(f"FOO env var = {foo}")
