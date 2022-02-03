dd784daf1e3be184eb49bd2f026037a4e03d02e0
Brian Sorahan <brian.sorahan@drillinginfo.com>
2022-02-03 10:18:58 -0600

    fix storage of slack flow

--- flows/slack_notification.py
+++ flows/slack_notification.py
@@ -1,4 +1,6 @@
 from prefect import Flow, task
+from prefect.run_configs.kubernetes import KubernetesRun
+from prefect.storage import Git
 from prefect.tasks.notifications.slack_task import SlackTask
 
 
@@ -8,4 +10,13 @@ def foo():
 
 
 with Flow(name="slack_notification") as flow:
+    flow.storage = Git(
+        repo_host="github.com",
+        repo="briansorahan/prefect-playground",
+        flow_path="flows/slack_notification.py",
+        branch_name="master",
+    )
+    flow.run_config = KubernetesRun(
+        image="devprismcr.azurecr.io/acuity/prefect/flows:0.0.69",
+    )
     foo_task = foo()
